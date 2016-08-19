dd4cdfca8530327e00dc966dea7dcd6c627a4f88
andy <a.chaa@marketinvoice.com>
2016-08-19 12:48:51 +0100

    frameless

--- main.js
+++ main.js
@@ -10,13 +10,10 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-  mainWindow.setMenu(null);
-
-  // and load the index.html of the app.
+  mainWindow = new BrowserWindow({width: 800, height: 600, frame: false})
+  // mainWindow = new BrowserWindow({width: 800, height: 600})
+  // mainWindow.setMenu(null);
   mainWindow.loadURL(`file://${__dirname}/index.html`)
-
-  // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
